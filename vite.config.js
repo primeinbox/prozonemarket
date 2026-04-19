fedd6814b3c1221c2acfd9679b93d4fe5c71c7a1
Suryadeep Singh <180669860+sudosuryadeep@users.noreply.github.com>
2026-04-19 14:18:54 +0530

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -2,10 +2,18 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-    server: {
-    hmr: false  // ← HMR band karo
+  build: {
+    chunkSizeWarningLimit: 1000,
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          vendor: ['react', 'react-dom', 'react-router-dom'],
+          motion: ['framer-motion'],
+          icons:  ['lucide-react'],
+        }
+      }
+    }
   }
 })
